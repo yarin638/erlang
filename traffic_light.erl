95355ba5100b0412173f834ce1beaee263eaa9af
yarin638 <88196663+yarin638@users.noreply.github.com>
2021-08-01 20:28:02 +0300

    Add files via upload

--- traffic_light.erl
+++ traffic_light.erl
@@ -10,7 +10,7 @@
 -author("yarinabutbul").
 -behaviour(gen_statem).
 
--export([start/0,push/0,get_count/0,stop/0]).
+-export([start/0,push/0,timeout/0,get_count/0,stop/0]).
 -export([terminate/3,code_change/4,init/1,callback_mode/0]).
 -export([green/3,red/3,yellow/3]).
 
@@ -22,6 +22,9 @@ start() ->
   gen_statem:start({local,name()}, ?MODULE, [], []).
 push() ->
   gen_statem:call(name(), push).
+
+timeout()-> gen_statem:call(name(),{time}). % timeout event
+
 get_count() ->
   gen_statem:call(name(), get_count).
 stop() ->
@@ -43,7 +46,14 @@ callback_mode() -> state_functions.
 red({call,From}, push, Data) ->
   %% Go to 'on', increment count and reply
   %% that the resulting status is 'on'
-  {next_state,yellow,Data+1,[{reply,From,yellow}]};
+  io:format("yellow~n"),
+  {next_state,yellow,Data+1,[{state_timeout,3000,lock}]};
+
+red(state_timeout, lock,  Data) ->
+  io:format("yellow~n"),
+  {next_state, yellow, Data, [{state_timeout,3000,lock}]};
+
+
 red(EventType, EventContent, Data) ->
   handle_event(EventType, EventContent, Data).
 
@@ -52,12 +62,22 @@ yellow({call,From}, push, Data) ->
   %% Go to 'on', increment count and reply
   %% that the resulting status is 'on'
   {next_state,green,Data+1,[{reply,From,green}]};
+
+yellow(state_timeout, lock,  Data) ->
+  io:format("green~n"),
+  {next_state, green, Data,[{state_timeout,3000,lock}]};
+
 yellow(EventType, EventContent, Data) ->
   handle_event(EventType, EventContent, Data).
 
 green({call,From}, push, Data) ->
   %% Go to 'off' and reply that the resulting status is 'off'
   {next_state,red,Data,[{reply,From,red}]};
+
+green(state_timeout, lock,  Data) ->
+  io:format("red~n"),
+  {next_state, red, Data,[{state_timeout,3000,lock}]};
+
 green(EventType, EventContent, Data) ->
   handle_event(EventType, EventContent, Data).
 
