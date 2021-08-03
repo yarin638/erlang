6a5833e868aeb8e0b119f3980a1e5a25930298ae
eliav2009 <88196806+eliav2009@users.noreply.github.com>
2021-08-03 12:44:36 +0300

    Add files via upload

--- Alerts.erl
+++ Alerts.erl
@@ -10,17 +10,62 @@
 -author("eliav").
 
 %% API
--export([tl_alert/2]).
+-export([tl_alert/2,car_alert/2]).
 car_alert(Car,'$end_of_table')->
   car_alert(Car,ets:first:(cars));
 
 car_alert(Car,P_car)->
-  [_CarNumber1,Road1,{Cx,Cy},Speed,Dir,_Color]=ets:lookup(cars,Car), %get car details
+  [_CarNumber1,Road1,{Cx1,Cy1},_Speed1,_Dir1,_Color1]=ets:lookup(cars,Car), %get car details
   Alive=ets:member(cars,P_car),
-  case Alive of
+  if
+    Alive=:=true->
+      [_CarNumber2,Road2,{Cx2,Cy2},Speed2,Dir2,_Color2]=ets:lookup(cars,P_car);
     true->
-      [_CarNumber,Road,{Cx,Cy},Speed,Dir,_Color]=ets:lookup(cars,P_car)
-  end
+      [_CarNumber2,Road2,{Cx2,Cy2},Speed2,Dir2,_Color2]=ets:lookup(cars,ets:first(cars))
+  end,
+  case Road1==Road2 of
+    true->
+      case Road1 of
+        north->
+          if
+            Cy2-Cy1<25 and Cy2-Cy1>0 ->
+              cars:close:car_alert(Car,P_car),
+              timer:sleep(500),
+              car_alert(Car,ets:first(cars));
+            true->
+              car_alert(Car,ets:next(cars,P_car))
+          end;
+        south->
+          if
+            Cy1-Cy2<25 and Cy1-Cy2>0 ->
+              cars:close:car_alert(Car,P_car),
+              timer:sleep(500),
+              car_alert(Car,ets:first(cars));
+            true->
+              car_alert(Car,ets:next(cars,P_car))
+          end;
+        west->
+          if
+            Cx1-Cx2<25 and Cx1-Cx2>0 ->
+              cars:close:car_alert(Car,P_car),
+              timer:sleep(500),
+              car_alert(Car,ets:first(cars));
+            true->
+              car_alert(Car,ets:next(cars,P_car))
+          end;
+        east->
+          if
+            Cx1-Cx2<25 and Cx1-Cx2>0 ->
+              cars:close:car_alert(Car,P_car),
+              timer:sleep(500),
+              car_alert(Car,ets:first(cars));
+            true->
+              car_alert(Car,ets:next(cars,P_car))
+          end
+      end;
+    false->
+      car_alert(Car,ets:next(cars,P_car))
+  end.
 
 
 tl_alert(Car,'$end_of_table')-> % start
@@ -33,7 +78,7 @@ tl_alert(Car,Junction)->
     true-> %same road
       case Dir of
         north-> %car is moving north
-          case Jy-Cy<25 orelse Jy-Cy>0 of   %check if car is near the junction, and didn't already passed it.
+          case Jy-Cy<25 and Jy-Cy>0 of   %check if car is near the junction, and didn't already passed it.
             true->
               cars:tl_alert(Car,sys:get_state(TLPid)),
               timer:sleep(1500),
@@ -42,7 +87,7 @@ tl_alert(Car,Junction)->
               tl_alert(Car,ets:next(traffic_light,Junction))
           end;
         south-> %car is moving north
-          case Cy-Jy<25 orelse Cy-Jy>0 of   %check if car is near the junction, and didn't already passed it.
+          case Cy-Jy<25 and Cy-Jy>0 of   %check if car is near the junction, and didn't already passed it.
             true->
               cars:tl_alert(Car,sys:get_state(TLPid)),
               timer:sleep(1500),
@@ -51,7 +96,7 @@ tl_alert(Car,Junction)->
               tl_alert(Car,ets:next(traffic_light,Junction))
           end;
         west-> %car is moving north
-          case Cx-Jx<25 orelse Cx-Jx>0 of   %check if car is near the junction, and didn't already passed it.
+          case Cx-Jx<25 and Cx-Jx>0 of   %check if car is near the junction, and didn't already passed it.
             true->
               cars:tl_alert(Car,sys:get_state(TLPid)),
               timer:sleep(1500),
@@ -60,7 +105,7 @@ tl_alert(Car,Junction)->
               tl_alert(Car,ets:next(traffic_light,Junction))
           end;
         east-> %car is moving north
-          case Jx-Cx<25 orelse Jx-Cx>0 of   %check if car is near the junction, and didn't already passed it.
+          case Jx-Cx<25 and Jx-Cx>0 of   %check if car is near the junction, and didn't already passed it.
             true->
               cars:tl_alert(Car,sys:get_state(TLPid)),
               timer:sleep(1500),
