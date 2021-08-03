874e6623e3f3d703f2299c6d68c1984cdb68605c
yarin638 <88196663+yarin638@users.noreply.github.com>
2021-08-03 20:45:47 +0300

    Add files via upload

---
+++ server.erl
@@ -0,0 +1,101 @@
+%%%-------------------------------------------------------------------
+%%% @author yarinabutbul
+%%% @copyright (C) 2021, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 03. Aug 2021 20:07
+%%%-------------------------------------------------------------------
+-module(server).
+-author("yarinabutbul").
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+  code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(server_state, {}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%% @doc Spawns the server and registers the local name (unique)
+-spec(start_link() ->
+  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%% @private
+%% @doc Initializes the server
+-spec(init(Args :: term()) ->
+  {ok, State :: #server_state{}} | {ok, State :: #server_state{}, timeout() | hibernate} |
+  {stop, Reason :: term()} | ignore).
+init([]) ->
+  ets:new(cars,[set,public,named_table]), ets:new(junction,[set,public,named_table]),
+  cars:start(yarin,1,1),cars:start(eliav,1,1),
+  cars:clear_path(yarin),
+  {ok, #server_state{}}.
+
+%% @private
+%% @doc Handling call messages
+-spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
+    State :: #server_state{}) ->
+  {reply, Reply :: term(), NewState :: #server_state{}} |
+  {reply, Reply :: term(), NewState :: #server_state{}, timeout() | hibernate} |
+  {noreply, NewState :: #server_state{}} |
+  {noreply, NewState :: #server_state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), Reply :: term(), NewState :: #server_state{}} |
+  {stop, Reason :: term(), NewState :: #server_state{}}).
+handle_call(_Request, _From, State = #server_state{}) ->
+  {reply, ok, State}.
+
+%% @private
+%% @doc Handling cast messages
+-spec(handle_cast(Request :: term(), State :: #server_state{}) ->
+  {noreply, NewState :: #server_state{}} |
+  {noreply, NewState :: #server_state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #server_state{}}).
+handle_cast(_Request, State = #server_state{}) ->
+  {noreply, State}.
+
+%% @private
+%% @doc Handling all non call/cast messages
+-spec(handle_info(Info :: timeout() | term(), State :: #server_state{}) ->
+  {noreply, NewState :: #server_state{}} |
+  {noreply, NewState :: #server_state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #server_state{}}).
+handle_info(_Info, State = #server_state{}) ->
+  {noreply, State}.
+
+%% @private
+%% @doc This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+-spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
+    State :: #server_state{}) -> term()).
+terminate(_Reason, _State = #server_state{}) ->
+  ok.
+
+%% @private
+%% @doc Convert process state when code is changed
+-spec(code_change(OldVsn :: term() | {down, term()}, State :: #server_state{},
+    Extra :: term()) ->
+  {ok, NewState :: #server_state{}} | {error, Reason :: term()}).
+code_change(_OldVsn, State = #server_state{}, _Extra) ->
+  {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
